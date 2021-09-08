d5090d6475f708097facf5f9e15efc2dfd185a87
JunChi <jun@junchis-mbp.lan>
2021-09-09 00:07:27 +0800

    successfully uploade files to IPFS and used Pinata to pin file

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -2,12 +2,14 @@ from brownie import AdvancedCollectible, network
 from metadata import sample_metadata
 from scripts.helpful_scripts import get_breed
 from pathlib import Path
+import os
+import requests
 
 def main ():
     print("Working on" + network.show_active())
     advanced_collectible = AdvancedCollectible[len(AdvancedCollectible) - 1]
     number_of_tokens = advanced_collectible.tokenCounter()
-    print("number of tokens you deployed is {number_of_tokens}")
+    print("number of tokens you deployed is " + str(number_of_tokens))
     write_metadata(number_of_tokens, advanced_collectible)
 
 def write_metadata(token_ids, nft_contract):
@@ -27,4 +29,21 @@ def write_metadata(token_ids, nft_contract):
                 nft_contract.tokenIdToBreed(token_id))
             collectible_metadata["description"] = "An adorable {} pup!".format(
                 collectible_metadata["name"])
-            print(collectible_metadata)
+            image_to_upload = None
+            if os.getenv("UPLOAD_IPFS") == "true":
+                image_path = "./img/{}.png".format(breed.lower().replace("_","-"))
+                image_to_upload = upload_to_ipfs(image_path)
+# # http://127.0.0.1:5001/webui
+
+# curl -X POST -F file=@img/{pug.png} http://localhost:5001/api/v0/add
+
+def upload_to_ipfs(filepath):
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        ipfs_url = "http://localhost:5001"
+        response = requests.post(ipfs_url + "/api/v0/add", files={"file": image_binary})
+        ipfs_hash = response.json()["Hash"]
+        filename = filepath.split("/")[-1:][0]
+        image_uri = "https://ipfs.io/ipfs/{}>filename={}".format(
+            ipfs_hash, filename)
+        print(image_uri)
